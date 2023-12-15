255f61ec97c15dc9af0c1090cc01087faec8bd3a
Douglas Alves <douglasmonttozo@hotmail.com.br>
2023-12-15 16:17:06 -0300

    inserindo dados no banco

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,22 @@
 import { Router } from "express";
-
+import mongodb from "mongodb";
 export const notesRouter = Router();
+import { ObjectId } from "mongodb";
+import { getDb } from "../db/connection.js";
 
 notesRouter.get("/", (req, res) => {
   res.render("notes/create");
+});
+
+//envio da nota para inserção no banco
+
+notesRouter.post("/", (req, res) => {
+  const data = req.body;
+  const title = data.title;
+  const description = data.description;
+  getDb()
+    .db()
+    .collection("notes")
+    .insertOne({ title: title, description: description });
+  res.redirect("/");
 });
