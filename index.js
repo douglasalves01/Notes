72eb5ccfbddef1788e39bc8081b677bb76a209bd
Douglas Alves <douglasmonttozo@hotmail.com.br>
2023-12-16 20:07:31 -0300

    exibindo tarefas na home

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import exphbs from "express-handlebars";
 import bodyParser from "body-parser";
 import { notesRouter } from "./routes/notes.js";
-import { initDb } from "./db/connection.js";
+import { getDb, initDb } from "./db/connection.js";
 
 const app = express();
 const port = 8000;
@@ -13,9 +13,11 @@ app.set("view engine", "handlebars");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 //rotas
-app.get("/", (req, res) => {
-  res.render("home");
+app.get("/", async (req, res) => {
+  const notes = await getDb().db().collection("notes").find({}).toArray();
+  res.render("home", { notes });
 });
+
 app.use("/notes", notesRouter);
 
 initDb((err) => {
